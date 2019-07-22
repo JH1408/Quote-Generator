518beb324a8c5133ef8d0a43a70e9f5eadfd7378
Josy Hartig <josy_hartig@yahoo.de>
2019-07-22 15:23:23 +0800

    undo last changes

--- index.js
+++ index.js
@@ -53,11 +53,10 @@ $(document).on('mouseover', '.daily-quote', function() {
     $('.quote-additional').css('transform', 'translateY(32px)');
   });
 
-$(document).on('mouseout', '.daily-quote', function() {
+$(document).on('mouseout', '.daily-quote', function(e) {
     $('.quote-additional').css('display', 'none');
     $('.quote').css('transform', 'translateY(0px)');
     $('.quote-additional').css('transform', 'translateY(0px)');
-    $('.liked-quotes-container').fadeOut();
   });
 
 // View liked quotes
