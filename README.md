da5bb01b7a6445f9b2e8257838a99bebd7ece410
Josy Hartig <josy_hartig@yahoo.de>
2019-09-25 09:50:34 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Quote Generator
+
+## Overview
+
+This app shows you inspirational quotes with dreamy background images.
+It is part of the FreeCodeCamp Front End Libraries Certification.
+
+See the project in action [here](https://jh1408.github.io/Quote-Generator/).
+
+## Features
+
+- Load quotes using the [Quotable API](https://github.com/lukePeavey/quotable)
+- Share quotes on Twitter
+- Load background images using [Unsplash Source](https://source.unsplash.com/)
+
+## Run the project
+
+It's as easy as this:
+- Clone the project
+- Open the index.html file in your browser
+
+## Tech stack
+- html
+- CSS
+- jQuery
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var savedHeart = window.localStorage.getItem('heart');
-var savedQuotes = window.localStorage.getItem('likedQuotes');
 
 $(document).ready(function() {
     $.getJSON('https://api.quotable.io/random', function(quote) {
@@ -7,15 +5,6 @@ $(document).ready(function() {
       $('.author').text(quote.author);
       $('.twitter a').attr('href', ($('.twitter a').attr('href') + quote.content + '  —  ' + quote.author));
   });
-});
-
-$(document).ready(function() {
-  if ("likedQuotes" in localStorage) {
-    $('.liked-quotes-container').html(savedQuotes);
-    $('.not-saved').remove();
-  } else {
-    $('.liked-quotes-container').append("<p class='not-saved' style='color: rgba(237, 237, 237, 0.6)'>You haven't liked any quotes yet</p>");
-  }
 });
 
 // Get a new quote
@@ -25,25 +14,6 @@ $('.refresh').click(function() {
       $('.author').text(quote.author);
       $('.twitter a').attr('href', ($('.twitter a').attr('href') + quote.content + '  —  ' + quote.author));
   });
-});
-
-// Like quote
-$(document).on('click', '.like', function() {
-  $('.like').removeClass('far like').addClass('fas liked');
-  window.localStorage.setItem('heart', $('.heart').html());
-  $('.liked-quotes-container').append('<p class="liked-quote">' + $('.quote').text() + ' <span>' + $('.author').text() + ' '+ ' </span><i class="fas fa-heart liked"></i></p><hr>');
-  window.localStorage.setItem('likedQuotes', $('.liked-quotes-container').html());
-  $('.not-saved').remove();
-});
-
-// Remove like
-$(document).on('click', '.liked', function() {
-  $('.liked').removeClass('fas liked').addClass('far like');
-  window.localStorage.removeItem('heart');
-  $('.liked-quote:first').remove();
-  $('hr:eq(1)').remove();
-  window.localStorage.setItem('likedQuotes', $('.liked-quotes-container').html());
-  $('.liked-quotes-container').append("<p class='not-saved' style='color: rgba(237, 237, 237, 0.6)'>You haven't liked any quotes yet.</p>");
 });
 
 // Hover over quote
@@ -58,18 +28,3 @@ $(document).on('mouseout', '.daily-quote', function(e) {
     $('.quote').css('transform', 'translateY(0px)');
     $('.quote-additional').css('transform', 'translateY(0px)');
   });
-
-// View liked quotes
-$('.liked-quotes').click(function(){
-  $('.liked-quotes-container').fadeToggle();
-  var offset = $(this).position();
-  var leftTotal = parseInt(offset.left, 10) - 23 +'px';
-  $('.liked-quotes-container').css('left', leftTotal);
-});
-
-// Hide when clicked outside
-$(document).mouseup(function (e){
-	if (!$('.liked-quotes-container').is(e.target) && !$('.liked-quotes').is(e.target)  && !$('.liked-quote').is(e.target) && !$('.quote-ellipsis').is(e.target) && !$('.like').is(e.target) && !$('.liked').is(e.target)){
-		$('.liked-quotes-container').fadeOut();
-  }
-});
